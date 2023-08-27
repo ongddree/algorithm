72beb1c752a5cacff6123049bad7c996946ff44b
seoyoung park <ongddree@bbros.kr>
2023-08-27 16:37:54 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -2,7 +2,6 @@ function solution(citations) {
    const sortedCitations = citations.sort((a,b) => b-a);
    let answer = 0;
    for (let i = 0; i < sortedCitations.length; i++) {
-     console.log(sortedCitations[i] ,i + 1);
      if (sortedCitations[i] >= i + 1) {
        answer = i + 1;
      }
